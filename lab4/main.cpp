73ab7037f4de3adef372e1411ada7c5f917d5378
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-20 14:09:18 +0300

    Добавьте файлы проекта.

---
+++ lab4/main.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2020 Petr Portnov <gh@progrm-jarvis.ru>
+
+#include <iostream>
+#include <string>
+int main(int n, char* arr[]) {
+	std::string path_to_ftp;
+	if (n != 1){
+		path_to_ftp = arr[1];
+	} else {
+		path_to_ftp = arr[0];
+	}
+	std::cout << path_to_ftp << std::endl;
+
+
+}
